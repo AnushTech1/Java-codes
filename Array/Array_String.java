b68f40d4c75f5a29df3ce2c4ad6803820e550701
AnushTech1 <anushbhise29@gmail.com>
2026-02-12 14:41:15 +0530

    Fixed Array folder and added all files

---
+++ Array/Array_String.java
@@ -0,0 +1,17 @@
+public class Array_String {
+    public static void main(String[] args) {
+        // This is a normal array example program
+        // String array
+        String[] courses = {
+            "Java",
+            "Python",
+            "C++",
+            "C#",
+            "JavaScript"
+        };
+        // Print the array elements using an enhanced for loop
+        for (String course : courses) {
+            System.out.println(course);
+        }
+    }
+}
